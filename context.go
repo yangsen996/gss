6e0b4732bf88651caaf855ea8583cfa0ade391e4
Leo <838725628@qq.com>
2021-12-17 14:25:02 +0800

    trie

--- context.go
+++ context.go
@@ -13,6 +13,7 @@ type Context struct {
 	Req        *http.Request
 	Path       string
 	Method     string
+	Params     map[string]string
 	StatusCode int
 }
 
@@ -24,7 +25,10 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 		Method: req.Method,
 	}
 }
-
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+}
 func (c *Context) PostForm(key string) string {
 	return c.Req.FormValue(key)
 }
--- router.go
+++ router.go
@@ -3,27 +3,80 @@ package gss
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandlerFunc)}
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+// parsePattern 解析路由
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
 
 func (r *router) addRouter(method string, pattern string, handle HandlerFunc) {
 	log.Printf("Router %4s - %s", method, pattern)
+	parts := parsePattern(pattern)
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	key := method + "-" + pattern
 	r.handlers[key] = handle
 }
 
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchpaths := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	n := root.search(searchpaths, 0)
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchpaths[index]
+			}
+			if part[0] == '*' && len(part) > 0 {
+				params[part[1:]] = strings.Join(searchpaths[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+	return nil, nil
+}
+
 func (r *router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c.Writer, c.Req)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c.Writer, c.Req)
+
 	} else {
-		c.String(http.StatusNotFound, "404 NOT FOUND:%s \n", c.Path)
+		c.String(http.StatusNotFound, "404 NOT FOUND %s\n", c.Path)
 	}
 }
